6efc6d53ab145c7d599778eb38265ad1b2d2d4eb
Yarin Nissan Avraham <yarinnissan1994@gmail.com>
2023-06-23 00:49:38 +0300

    Initial commit.

---
+++ src/main/java/com/shopping_cli/Application.java
@@ -0,0 +1,15 @@
+package com.shopping_cli;
+
+import com.google.gson.Gson;
+import com.shopping_cli.entities.User;
+import com.shopping_cli.entities.UserType;
+import com.shopping_cli.services.HttpClientService;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
