923b34aa1e13eb3e4048ff5b8f329d0b4d31c5cc
Yarin Nissan Avraham <yarinnissan1994@gmail.com>
2023-06-23 05:51:17 +0300

    Created application EntryMenu.

---
+++ src/main/java/com/shopping_cli/Menus/EntryMenu.java
@@ -0,0 +1,55 @@
+package com.shopping_cli.Menus;
+
+import com.shopping_cli.services.ConsoleService;
+
+import java.util.Scanner;
+
+public class EntryMenu {
+    public static int start(Scanner scanner) {
+
+        ConsoleService.clearConsole();
+
+        boolean breakLoop;
+
+        int value;
+
+        do {
+            breakLoop = true;
+
+            System.out.println("==========================================");
+            System.out.println("|        Welcome to Shopping CLI         |");
+            System.out.println("==========================================");
+            System.out.println("|              Entry Menu                |");
+            System.out.println("==========================================");
+            System.out.println("|  1. Register                           |");
+            System.out.println("|  2. Login                              |");
+            System.out.println("|  0. Exit                               |");
+            System.out.println("==========================================");
+
+            System.out.print("Enter your choice: ");
+            String choice = scanner.nextLine();
+
+             value = switch (choice) {
+                case "1" -> 1;
+                case "2" -> 2;
+                case "0" -> {
+                    System.out.println("Exiting...");
+                    yield 0;
+                }
+                default -> {
+                    ConsoleService.spacingConsole();
+                    System.out.println("Invalid choice. Please try again.");
+                    System.out.print("Press Enter to continue...");
+                    scanner.nextLine();
+                    ConsoleService.spacingConsole();
+                    breakLoop = false;
+                    yield 0;
+                }
+            };
+
+            System.out.println();
+        } while (!breakLoop);
+
+        return value;
+    }
+}
