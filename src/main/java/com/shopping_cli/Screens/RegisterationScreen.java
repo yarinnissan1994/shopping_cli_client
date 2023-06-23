edd9ae1dc28552a7a9d7a20764292367d9db3fae
Yarin Nissan Avraham <yarinnissan1994@gmail.com>
2023-06-23 05:51:58 +0300

    Created Registration Screen.

---
+++ src/main/java/com/shopping_cli/Screens/RegisterationScreen.java
@@ -0,0 +1,44 @@
+package com.shopping_cli.Screens;
+
+import com.shopping_cli.services.ConsoleService;
+
+import java.util.Scanner;
+
+public class RegisterationScreen {
+    public static void start(Scanner scanner) {
+        ConsoleService.clearConsole();
+
+        boolean validCredentials = false;
+
+        do {
+            ConsoleService.spacingConsole();
+
+            System.out.println("==========================================");
+            System.out.println("|              Shopping CLI              |");
+            System.out.println("==========================================");
+            System.out.println("|            Registration Page           |");
+            System.out.println("==========================================");
+
+            System.out.print("Enter name: ");
+            String name = scanner.nextLine();
+
+            System.out.print("Enter Email: ");
+            String email = scanner.nextLine();
+
+            System.out.print("Enter Password: ");
+            String password = scanner.nextLine();
+
+
+            if (email.equals("") && password.equals("") && name.equals("")) {
+                validCredentials = true;
+            } else {
+                ConsoleService.spacingConsole();
+                System.out.println("Invalid credentials. Please try again.");
+                System.out.print("Press Enter to continue...");
+                scanner.nextLine();
+            }
+
+            System.out.println();
+        } while (!validCredentials);
+    }
+}
