d8abc7679bf9880b2c4d165ee9ccfc7bb1fe70e1
Yarin Nissan Avraham <yarinnissan1994@gmail.com>
2023-06-24 19:07:41 +0300

    Adjustment - some improvements for the login screen.

--- src/main/java/com/shopping_cli/Screens/LoginScreen.java
+++ src/main/java/com/shopping_cli/Screens/LoginScreen.java
@@ -1,13 +1,19 @@
 package com.shopping_cli.Screens;
 
+import com.shopping_cli.Application;
+import com.shopping_cli.entities.User;
 import com.shopping_cli.services.ConsoleService;
+import com.shopping_cli.data.UserData;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Scanner;
 
 public class LoginScreen {
     public static void start(Scanner scanner) {
 
-        boolean loggedIn = false;
+        boolean breakLoop = false;
+        String choice;
 
         do {
             ConsoleService.clearConsole();
@@ -17,6 +23,14 @@ public class LoginScreen {
             System.out.println("==========================================");
             System.out.println("|               Login Page               |");
             System.out.println("==========================================");
+            System.out.println();
+            System.out.print("Press Enter to continue login or type 0 to exit: ");
+
+            choice = scanner.nextLine();
+
+            if(choice.equals("0")) return;
+
+            ConsoleService.spacingConsole();
 
             System.out.print("Enter Email: ");
             String email = scanner.nextLine();
@@ -24,16 +38,26 @@ public class LoginScreen {
             System.out.print("Enter Password: ");
             String password = scanner.nextLine();
 
-            if (email.equals("") && password.equals("")) {
-                loggedIn = true;
-            } else {
-                ConsoleService.clearConsole();
-                System.out.println("Invalid credentials. Please try again.");
-                System.out.print("Press Enter to continue...");
-                scanner.nextLine();
+            User user = new User();
+            user.setEmail(email);
+            user.setPassword(password);
+
+            try {
+                int responseCode = UserData.login(user, Application.sessionToken);
+                ConsoleService.spacingConsole();
+                if (responseCode == 202) {
+                    System.out.print("Press Enter to continue...");
+                    scanner.nextLine();
+                    breakLoop = true;
+                } else {
+                    System.out.print("Press Enter to continue...");
+                    scanner.nextLine();
+                }
+            } catch (URISyntaxException | IOException | InterruptedException e) {
+                throw new RuntimeException(e);
             }
 
             System.out.println();
-        } while (!loggedIn);
+        } while (!breakLoop);
     }
 }
