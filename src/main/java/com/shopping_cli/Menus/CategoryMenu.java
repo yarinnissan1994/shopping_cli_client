2746381f4dc11db6914cfcb02bdd2d641860fb6f
Yarin Nissan Avraham <yarinnissan1994@gmail.com>
2023-06-25 18:43:38 +0300

    optimized menus.

--- src/main/java/com/shopping_cli/Menus/CategoryMenu.java
+++ src/main/java/com/shopping_cli/Menus/CategoryMenu.java
@@ -6,19 +6,29 @@ import com.shopping_cli.entities.Category;
 import com.shopping_cli.services.ConsoleService;
 import com.shopping_cli.services.ListManagerService;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class CategoryMenu {
     public static void start(Scanner scanner) {
+        ConsoleService.clearConsole();
+
         List<Category> categories = CategoryData.getAllCategories();
+
         ListManagerService<Category> categoryListManager = new ListManagerService<>(categories);
+
+        boolean breakLoop;
+
         boolean isFind = false;
+
         String choice;
 
         do {
-            ConsoleService.clearConsole();
+            breakLoop = false;
+
+            List<Category> currentPageItems = categoryListManager.getCurrentPageItems();
             int currentPage = categoryListManager.getCurrentPage();
             int totalPages = categoryListManager.getTotalPages();
 
@@ -28,17 +38,17 @@ public class CategoryMenu {
             System.out.println("|              Categories                |");
             System.out.println("==========================================");
             System.out.println("| A. All Categories                      |");
-            if (currentPage < totalPages)
+            if(currentPage < totalPages )
                 System.out.println("| N. Next Page                           |");
-            if (currentPage != 1)
+            if(currentPage != 1)
                 System.out.println("| P. Previous Page                       |");
-            if (isFind)
+            if(isFind)
                 System.out.println("| R. Return to all categories            |");
             System.out.println("| F. Find Category                       |");
             System.out.println("| 0. Back to Main Menu                   |");
             System.out.println("==========================================");
 
-            displayCategories(categoryListManager.getCurrentPageItems(), currentPage, totalPages);
+            displayCategories(currentPageItems, currentPage, totalPages);
 
             System.out.print("Enter your choice: ");
             choice = scanner.nextLine().toLowerCase();
@@ -46,9 +56,11 @@ public class CategoryMenu {
             switch (choice) {
                 case "n" -> {
                     categoryListManager.goToNextPage();
+                    ConsoleService.clearConsole();
                 }
                 case "p" -> {
                     categoryListManager.goToPreviousPage();
+                    ConsoleService.clearConsole();
                 }
                 case "r" -> {
                     categoryListManager = new ListManagerService<>(categories);
@@ -58,18 +70,29 @@ public class CategoryMenu {
                     System.out.print("Search Term: ");
                     String searchTerm = scanner.nextLine();
                     System.out.println("Searching for " + searchTerm + "...");
-                    List<Category> filteredCategories = categories.stream()
-                            .filter(category -> category.getName().toLowerCase().contains(searchTerm.toLowerCase()))
-                            .collect(Collectors.toList());
-                    categoryListManager = new ListManagerService<>(filteredCategories);
+                    categoryListManager = new ListManagerService<>(categories
+                            .stream()
+                            .filter(category -> category
+                                    .getName()
+                                    .toLowerCase()
+                                    .contains(searchTerm.toLowerCase()))
+                            .collect(Collectors.toList()));
                     isFind = true;
+                    ConsoleService.clearConsole();
                 }
                 case "a" -> {
-                    ProductMenu.start(scanner, ProductData.getAllProducts(), "All Categories");
+                    ProductMenu.start(scanner,
+                            ProductData.getAllProducts(),
+                            "All Categories");
+                }
+                case "1", "2", "3", "4", "5" -> {
+                    ProductMenu.start(scanner,
+                            ProductData.getAllProductsByCategory(currentPageItems.get(Integer.parseInt(choice) - 1).getId()),
+                            currentPageItems.get(0).getName());
                 }
                 case "0" -> {
                     System.out.println("Exiting...");
-                    return;
+                    breakLoop = true;
                 }
                 default -> {
                     ConsoleService.spacingConsole();
@@ -78,15 +101,18 @@ public class CategoryMenu {
                     scanner.nextLine();
                 }
             }
-        } while (true);
+        } while (!breakLoop);
     }
 
+
     private static void displayCategories(List<Category> categories, int currentPage, int totalPages) {
+
         System.out.println();
         System.out.println("Page " + currentPage + " of " + totalPages);
         System.out.println();
 
         int i = 1;
+
         for (Category category : categories) {
             System.out.println("- " + i + ". " + category.getName());
             i++;
--- src/main/java/com/shopping_cli/Menus/ProductMenu.java
+++ src/main/java/com/shopping_cli/Menus/ProductMenu.java
@@ -32,13 +32,13 @@ public class ProductMenu {
             System.out.println("==========================================");
             dynamicMenuHeader(categoryName);
             System.out.println("==========================================");
-            if (currentPage < totalPages)
+            if(currentPage < totalPages )
                 System.out.println("| N. Next Page                           |");
-            if (currentPage != 1)
+            if(currentPage != 1)
                 System.out.println("| P. Previous Page                       |");
             System.out.println("| SN. Sort Products by name              |");
             System.out.println("| SP. Sort Products by price             |");
-            if (isFind)
+            if(isFind)
                 System.out.println("| R. Return to all Products              |");
             System.out.println("| F. Search Product                      |");
             System.out.println("| 0. Back to Categories                  |");
@@ -46,7 +46,7 @@ public class ProductMenu {
 
             displayProducts(currentPageItems, currentPage, totalPages);
 
-            if (products.size() != 0) {
+            if(products.size() != 0){
                 System.out.print("Enter your choice: ");
                 choice = scanner.nextLine().toLowerCase();
             } else {
@@ -81,6 +81,10 @@ public class ProductMenu {
                     isFind = true;
                     ConsoleService.clearConsole();
                 }
+                case "1", "2", "3", "4", "5" -> {
+                    ProductScreen.start(scanner, currentPageItems.get(Integer.parseInt(choice) - 1));
+                    breakLoop = true;
+                }
                 case "0" -> {
                     System.out.println("Exiting...");
                     breakLoop = true;
@@ -102,15 +106,17 @@ public class ProductMenu {
         System.out.println("Page " + currentPage + " of " + totalPages);
         System.out.println();
 
-        if (products.isEmpty()) {
+        int i = 1;
+
+        if (products.size() == 0) {
             System.out.println("No products found.");
             ConsoleService.sleep(1000);
             return;
         }
 
-        for (int i = 0; i < products.size(); i++) {
-            Product product = products.get(i);
-            System.out.println("- " + (i + 1) + ". " + product.getName() + " - " + product.getPrice() + "$");
+        for (Product product : products) {
+            System.out.println("- " + i + ". " + product.getName() + " - " + product.getPrice() +"$");
+            i++;
         }
 
         System.out.println("==========================================");
@@ -123,9 +129,9 @@ public class ProductMenu {
 
         StringBuilder sb = new StringBuilder();
         sb.append("|");
-        sb.append(" ".repeat(Math.max(0, padding)));
+        sb.append(" " .repeat(Math.max(0, padding)));
         sb.append(categoryName);
-        sb.append(" ".repeat(Math.max(0, padding)));
+        sb.append(" " .repeat(Math.max(0, padding)));
         sb.append("|");
 
         String paddedString = sb.toString();
